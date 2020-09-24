91b82774b9d226dc6fbc7a9dc998a0cf467b7dfb
Iso <kagiso.k.mashigo@gmial.com>
2020-09-24 21:54:24 +0000

    index file now working

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,9 +1,21 @@
 const request = require('request');
 const breedSearch = process.argv[2];
 
+const fetchBreedDescription = (breedSearch, callBack) => {
 request(`https://api.thecatapi.com/v1/breeds/search/?q=${breedSearch}`, (error, response, body) => {
-
+  if (error) {
+    return callBack(error, null);
+  }
   const data = JSON.parse(body);
-  console.log("Cat Breed: ", data[0].description);
+  
+  if (!data[0]) {
+    const noBreedErr = `Sorry there's no description for ${breedSearch}!`
+    return callBack(noBreedErr, null);
+  } else if (data[0]) {
+    const description = data[0].description; // putting this line here protects (in the sense that it will establish that it actually exists)
+    return callBack(null, description);
+  }
+  });
+}
 
-});
+module.exports = { fetchBreedDescription };
